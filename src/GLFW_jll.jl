9de83cc9e2113173c2178a014961ab4a7a2db93e
Elliot Saba <staticfloat@gmail.com>
2019-10-17 13:29:05 -0400

    GLFW_jll build 3.3.0+0

---
+++ src/GLFW_jll.jl
@@ -0,0 +1,31 @@
+module GLFW_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("0656b61e-2033-5cc2-a64a-77c0f6c09b89"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "GLFW", artifacts_toml) for e in artifacts["GLFW"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load GLFW; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module GLFW_jll
