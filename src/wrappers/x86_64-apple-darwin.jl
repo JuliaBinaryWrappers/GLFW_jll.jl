3bef4459b63faabee13c2f6290c1da70a2accd95
jlbuild <juliabuildbot@gmail.com>
2024-07-01 10:21:07 +0000

    GLFW_jll build 3.4.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -7,7 +7,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libglfw,
-        "lib/libglfw.3.3.dylib",
+        "lib/libglfw.3.4.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
