cd6bc719947a0697c1d2af057a8870ae74590088
jlbuild <juliabuildbot@gmail.com>
2025-12-08 04:20:45 +0000

    GLFW_jll build 3.4.0+3

--- src/GLFW_jll.jl
+++ src/GLFW_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("GLFW")
-JLLWrappers.@generate_main_file("GLFW", UUID("0656b61e-2033-5cc2-a64a-77c0f6c09b89"))
+JLLWrappers.@generate_main_file("GLFW", Base.UUID("0656b61e-2033-5cc2-a64a-77c0f6c09b89"))
 end  # module GLFW_jll
